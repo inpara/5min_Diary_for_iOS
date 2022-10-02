84efef59989ed479e8d466780953f726c80a4399
inpara <84673263+inpara@users.noreply.github.com>
2022-10-02 18:44:09 +0900

    簡易Todoリスト用のサンプルコードを適用

--- 5min_Diary_for_iOS/ContentView.swift
+++ 5min_Diary_for_iOS/ContentView.swift
@@ -7,19 +7,100 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext//環境変数managedObjectContextを取得し、viewContextに格納
-    
-    
-    
+ 
+    /// 被管理オブジェクトコンテキスト（ManagedObjectContext）の取得
+    @Environment(\.managedObjectContext) private var context
+ 
+    /// データ取得処理
+    @FetchRequest(
+        entity: Task.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Task.timestamp, ascending: true)],
+        predicate: nil
+    ) private var tasks: FetchedResults<Task>
+   
     var body: some View {
-        Text("Hello CoreData!!")
+        NavigationView {
+            
+            /// 取得したデータをリスト表示
+            List {
+                ForEach(tasks) { task in
+                    
+                    /// タスクの表示
+                    HStack {
+                        Image(systemName: task.checked ? "checkmark.circle.fill" : "circle")
+                        Text("\(task.name!)")
+                        Spacer()
+                    }
+                    
+                    /// タスクをタップでcheckedフラグを変更する
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        task.checked.toggle()
+                        try? context.save()
+                    }
+                }
+                .onDelete(perform: deleteTasks)
+            }
+            .navigationTitle("Todoリスト")
+            
+            /// ツールバーの設定
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: AddTaskView()) {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        }
     }
     
-    
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
+    /// タスクの削除
+    /// - Parameter offsets: 要素番号のコレクション
+    func deleteTasks(offsets: IndexSet) {
+        for index in offsets {
+            context.delete(tasks[index])
         }
+        try? context.save()
+    }
+}
+ 
+/// タスク追加View
+struct AddTaskView: View {
+    @Environment(\.managedObjectContext) private var context
+    @Environment(\.presentationMode) var presentationMode
+    @State private var task = ""
+    
+    var body: some View {
+        Form {
+            Section() {
+                TextField("タスクを入力", text: $task)
+            }
+        }
+        .navigationTitle("タスク追加")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("保存") {
+                    /// タスク新規登録処理
+                    let newTask = Task(context: context)
+                    newTask.timestamp = Date()
+                    newTask.checked = false
+                    newTask.name = task
+                    
+                    try? context.save()
+ 
+                    /// 現在のViewを閉じる
+                    presentationMode.wrappedValue.dismiss()
+                }
+            }
+        }
+    }
+}
+ 
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
